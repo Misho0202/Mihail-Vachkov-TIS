68fb01e886fe8e52ff5f28c4559517ec6435a298
Misho0202 <mishi.vachkov@gmail.com>
2025-01-12 12:54:42 +0200

    New

---
+++ Mihail-Vachkov-TIS-Setup/MovieStoreC/MovieStoreC.BL/Services/MoviesService.cs
@@ -0,0 +1,33 @@
+﻿using MovieStoreC.BL.Interfaces;
+using MovieStoreC.DL.Interfaces;
+using MovieStoreC.Models.DTO;
+
+namespace MovieStoreC.BL.Services
+{
+    internal class MoviesService : IMoviesService
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public MoviesService(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        public void Add(Movie movie)
+        {
+            //_movieRepository.Add(movie);
+        }
+
+        public List<Movie> GetAll()
+        {
+            return _movieRepository.GetAll();
+        }
+
+        public Movie? GetById(string id)
+        {
+            if (string.IsNullOrEmpty(id)) return null;
+
+            return _movieRepository.GetById(id);
+        }
+    }
+}
---
+++ Mihail-Vachkov-TIS-Setup/MovieStoreC/MovieStoreC.DL/Interfaces/IMovieRepository.cs
@@ -0,0 +1,11 @@
+﻿using MovieStoreC.Models.DTO;
+
+namespace MovieStoreC.DL.Interfaces
+{
+    public interface IMovieRepository
+    {
+        List<Movie> GetAll();
+
+        Movie? GetById(string id);
+    }
+}
